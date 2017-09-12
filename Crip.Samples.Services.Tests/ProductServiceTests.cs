ca0829b9719e01dab18093eb7eca77468b5d633e
tahq69 <tahq69@gmail.com>
2017-09-12 15:26:08 +0300

    Service test project and first tests added;

---
+++ Crip.Samples.Services.Tests/ProductServiceTests.cs
@@ -0,0 +1,35 @@
+﻿namespace Crip.Samples.Services.Tests
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using System.Linq;
+
+    [TestClass]
+    public partial class ProductServiceTests
+    {
+        private ProductService svc;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            this.svc = new ProductService();
+        }
+
+        [TestMethod]
+        public void Test_Product_AllShouldReturnCollectionOfProducts()
+        {
+            var result = this.svc.All();
+
+            Assert.IsNotNull(result, "Method 'All' returned null instead of collection");
+            Assert.IsTrue(result.Count() > 0, "Result count should be greater than a 0");
+        }
+
+        [TestMethod]
+        public void Test_Product_FindShouldreturnrecordWithCorrectIdentifier()
+        {
+            var result = this.svc.Find(2);
+
+            Assert.IsNotNull(result, "Method 'Find' returned null instead of record");
+            Assert.AreEqual(2, result.Id);
+        }
+    }
+}
---
+++ Crip.Samples.Services.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Crip.Samples.Services.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Crip.Samples.Services.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4aa95ccf-9cc9-44e8-ab82-506d95627185")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
